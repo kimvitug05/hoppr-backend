2d38eca582c1389eeda878c755c5a5d083076457
Kimberly Vitug <kim_vitug05@yahoo.com>
2021-02-16 00:53:05 -0800

    load google application credentials from environment variable

--- src/main/java/xyz/hoppr/hoppr/config/FirebaseConfig.java
+++ src/main/java/xyz/hoppr/hoppr/config/FirebaseConfig.java
@@ -3,20 +3,27 @@ package xyz.hoppr.hoppr.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
 
 @Configuration
 public class FirebaseConfig {
+
+    @Value("${GOOGLE_APPLICATION_CREDENTIALS}")
+    private String googleApplicationCredentials;
 
     @Bean
     @Primary
     public void firebaseInit() throws IOException {
         FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.getApplicationDefault())
+                .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(Base64.getDecoder().decode(googleApplicationCredentials))))
                 .build();
         if (FirebaseApp.getApps().isEmpty()) {
             FirebaseApp.initializeApp(options);
